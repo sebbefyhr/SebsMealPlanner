ebb07ad73d9423c225ec8631c3ffbff873a1d586
sebbefyhr <sebbe,fyhr@hotmail.com>
2024-02-05 14:47:01 +0100

    added fooditem module

---
+++ FoodItemMicroService/src/main/java/org/fyhr/FoodItem.java
@@ -0,0 +1,11 @@
+package org.fyhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodItem {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodItem.class, args);
+    }
+}
